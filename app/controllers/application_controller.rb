24501f6e131d209a52f7f7d0e64660d4c3719f02
Nicole Candiotti <nccandiotti@gmail.com>
2022-03-04 10:53:12 -0500

    commit tree 0b4e24cd53a48ace0306d3312f5b9d1340232795

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,34 @@
 class ApplicationController < Sinatra::Base
+  set :default_content_type, 'application/json'
+  get '/games' do
+    # get all the games from the database
+    games = Game.all
+    # return a JSON response with an array of all the game data
+    games.to_json
+  end
 
-  get '/' do
-    { message: "Hello world" }.to_json
+  get '/games/:id' do
+    # look up the game in the database using its ID
+    game = Game.find(params[:id])
+    # send a JSON-formatted response of the game data
+    # game.to_json
+     # include associated reviews in the JSON response
+     game.to_json(include:  { reviews: { include: :user } })
   end
 
 end
+
+
+# class ApplicationController < Sinatra::Base
+
+#   get '/games' do
+
+#     Game.all
+
+#     # get all the games from the database
+#     # return a JSON response with an array of all the game data
+#        # return a JSON response with an array of all the game data
+#        games.to_json
+#   end
+
+# end
